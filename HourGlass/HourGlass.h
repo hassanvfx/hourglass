94eba1fa99013cd9da8eacab17cf8227617ddc4c
hassan uriostegui <hassanvfx@gmail.com>
2018-09-07 05:47:49 -0700

    first commit

---
+++ HourGlass/HourGlass.h
@@ -0,0 +1,19 @@
+//
+//  HourGlass.h
+//  HourGlass
+//
+//  Created by hassan uriostegui on 9/7/18.
+//  Copyright © 2018 FxReactor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HourGlass.
+FOUNDATION_EXPORT double HourGlassVersionNumber;
+
+//! Project version string for HourGlass.
+FOUNDATION_EXPORT const unsigned char HourGlassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HourGlass/PublicHeader.h>
+
+
---
+++ HourGlass/HourGlass.swift
@@ -0,0 +1,13 @@
+//
+//  HourGlass.swift
+//  HourGlass
+//
+//  Created by hassan uriostegui on 9/7/18.
+//  Copyright © 2018 FxReactor. All rights reserved.
+//
+
+import UIKit
+
+class HourGlass: NSObject {
+
+}
